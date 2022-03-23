bb2c6b5c5748dff7243b528032a94fa28563da5b
emkis <nicolasemkis@gmail.com>
2022-03-23 00:33:23 -0300

    chore: move release config to release config file

---
+++ release.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/npm',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'ci', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'BREAKING_CHANGE', release: 'major' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
