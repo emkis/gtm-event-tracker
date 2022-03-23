1b0868ce9e24bc97e5366bddd110f29318826f42
emkis <nicolasemkis@gmail.com>
2022-03-23 00:37:38 -0300

    feat: add release configurations

--- release.config.js
+++ release.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/npm',
     [
       '@semantic-release/commit-analyzer',
       {
@@ -18,13 +17,16 @@ module.exports = {
         ],
       },
     ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'],
+        assets: ['package.json', 'CHANGELOG.md'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
+      }
     ],
+    '@semantic-release/npm'
   ],
 }
