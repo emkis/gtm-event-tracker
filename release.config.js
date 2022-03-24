278bc64c1a1aba5018f84fa04356326a05cd27d1
emkis <nicolasemkis@gmail.com>
2022-03-23 21:40:06 -0300

    fix: improve the release commit note

--- release.config.js
+++ release.config.js
@@ -21,8 +21,7 @@ module.exports = {
     [
       '@semantic-release/git', {
         assets: ['package.json', 'CHANGELOG.md'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        message: 'chore(release): v${nextRelease.version}',
       }
     ],
     [
