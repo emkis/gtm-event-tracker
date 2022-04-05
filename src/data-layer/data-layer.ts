0ff81628782f6bef9d22f53cab23f0eaf8514f55
emkis <nicolasemkis@gmail.com>
2022-04-05 00:27:15 -0300

    fix: make sure that environment dont break

--- src/data-layer/data-layer.ts
+++ src/data-layer/data-layer.ts
@@ -15,13 +15,13 @@ export function createDataLayerModule(): DataLayerModule {
   }
 
   function assertIsAvailable() {
-    const isServer = typeof window === 'undefined'
-    const isDefined = typeof window.dataLayer !== 'undefined'
-    const isArray = Array.isArray(window.dataLayer)
+    const isServer = () => typeof window === 'undefined'
+    const isDefined = () => typeof window.dataLayer !== 'undefined'
+    const isArray = () => Array.isArray(window.dataLayer)
 
-    if (isServer) throwIsServer()
-    else if (!isDefined) throwIsNotDefined()
-    else if (!isArray) throwIsNotArray()
+    if (isServer()) throwIsServer()
+    else if (!isDefined()) throwIsNotDefined()
+    else if (!isArray()) throwIsNotArray()
   }
 
   return {
