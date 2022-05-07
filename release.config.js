7809953f417c5307ffee54e1b89a3915aea0ca4a
emkis <nicolasemkis@gmail.com>
2022-05-06 23:28:03 -0300

    chore(release): change release commit type

--- release.config.js
+++ release.config.js
@@ -25,7 +25,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        message: 'chore(release): v${nextRelease.version}',
+        message: 'release: v${nextRelease.version}',
       },
     ],
     [
