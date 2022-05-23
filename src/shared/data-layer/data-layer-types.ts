417ad43b964473f8581be68cfa6ee6d1c9ee23c1
emkis <nicolasemkis@gmail.com>
2022-05-22 22:54:42 -0300

    refactor(data-layer): stop sharing unnecessary method

--- src/shared/data-layer/data-layer-types.ts
+++ src/shared/data-layer/data-layer-types.ts
@@ -8,17 +8,9 @@ export type EventProperties = Record<string, string | number>
  * The type-safe and recommended way to interact with `window.dataLayer`.
  * @internal
  */
-export type DataLayerFunctions = Readonly<{
+export type DataLayer = Readonly<{
   /**
    * Pushes an event to the target array.
    */
   addEvent: (payload: EventProperties) => void
-
-  /**
-   * Checks if the targetProperty is available.
-   *
-   * This array is injected in the `window` by Google Tag Manager, so you
-   * just need to have this dependency installed correctly.
-   */
-  checkTargetPropertyAvailability: () => void
 }>
