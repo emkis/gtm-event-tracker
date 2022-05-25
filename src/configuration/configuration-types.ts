fb2589f3bacce16d8e3965c0d056d9f75bbccb3d
emkis <nicolasemkis@gmail.com>
2022-05-24 23:16:11 -0300

    refactor(configuration): make options required

--- src/configuration/configuration-types.ts
+++ src/configuration/configuration-types.ts
@@ -4,11 +4,11 @@ import type { EventProperties } from '@/shared/data-layer'
  * Available options for the logger.
  * @public
  */
-export type LoggerConfigurations = Partial<{
+export type LoggerConfigurations = {
   debugAll: boolean
   debugEvents: boolean
   debugContext: boolean
-}>
+}
 
 /**
  * Available options for events.
@@ -22,7 +22,7 @@ export type EventsConfigurations = {
  * All available configuration options.
  * @public
  */
-export type Configurations = Partial<{
+export type Configurations = {
   logger: LoggerConfigurations
   events: EventsConfigurations
-}>
+}
