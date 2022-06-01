89f4224ecd5399ee65521b0c0ff4336f40c51ab3
emkis <nicolasemkis@gmail.com>
2022-05-31 23:56:31 -0300

    chore(rollup): add bundle configuration

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+import size from 'rollup-plugin-size'
+
+/** @type {import('rollup').RollupOptions} */
+const config = {
+  input: 'src/index.ts',
+  plugins: [typescript(), nodeResolve(), size({ gzip: true })],
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'commonjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.min.js',
+      format: 'umd',
+      name: 'GTMEventTracker',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+}
+
+export default config
