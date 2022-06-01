09dd873ba4f020be456a355998f86271d82ca1c3
emkis <nicolasemkis@gmail.com>
2022-06-01 01:15:08 -0300

    chore(jest): allow .test files only

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   clearMocks: true,
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test|tests).[jt]s?(x)',
-  ],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
   modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/node_modules'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
