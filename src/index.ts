6cf6f16155a968b08278f8d65daeed50fb51f104
emkis <nicolasemkis@gmail.com>
2022-06-01 23:14:40 -0300

    refactor: change export to be compatible with isolatedModules

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-export {
-  createTrackerContext,
-  TrackerContext,
-  TrackerContextOptions,
-} from '@/tracker-context'
-
-export {
-  withTrackerContext,
-  TrackModule,
-  SubtractEventProperties,
-} from '@/with-tracker'
-
 export type { EventProperties } from '@/shared/data-layer'
-export { setLogger, Logger, LoggerAction } from '@/logger'
 
-export {
-  configure,
+export { createTrackerContext } from '@/tracker-context'
+export type { TrackerContext, TrackerContextOptions } from '@/tracker-context'
+
+export { withTrackerContext } from '@/with-tracker'
+export type { TrackModule, SubtractEventProperties } from '@/with-tracker'
+
+export { setLogger } from '@/logger'
+export type { Logger, LoggerAction } from '@/logger'
+
+export { configure } from '@/configuration'
+export type {
   Configurations,
   LoggerConfigurations,
   EventsConfigurations,
