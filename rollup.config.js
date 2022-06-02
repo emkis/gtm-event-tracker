cbf148060369ee3045d0958a45307ca393cc6dae
emkis <nicolasemkis@gmail.com>
2022-06-02 00:34:42 -0300

    chore(rollup): remove umd bundle output

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,11 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
 import size from 'rollup-plugin-size'
 
 /** @type {import('rollup').RollupOptions} */
 const config = {
   input: 'src/index.ts',
-  plugins: [typescript(), commonjs(), nodeResolve(), size({ gzip: true })],
   output: [
     {
       file: 'dist/index.cjs.js',
@@ -19,14 +17,8 @@ const config = {
       format: 'es',
       sourcemap: true,
     },
-    {
-      file: 'dist/index.min.js',
-      format: 'umd',
-      name: 'GTMEventTracker',
-      sourcemap: true,
-      plugins: [terser()],
-    },
   ],
+  plugins: [typescript(), commonjs(), nodeResolve(), size({ gzip: true })],
 }
 
 export default config
