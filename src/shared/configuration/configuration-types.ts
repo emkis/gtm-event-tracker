a0f927d819f6eadebdb51afc43098c04c62faeb2
emkis <nicolasemkis@gmail.com>
2022-06-13 22:44:47 -0300

    refactor(configuration): change configuration to shallow object

--- src/shared/configuration/configuration-types.ts
+++ src/shared/configuration/configuration-types.ts
@@ -1,28 +1,12 @@
 import type { EventProperties } from '@/shared/data-layer'
 
 /**
- * Available options for the logger.
- * @public
- */
-export type LoggerConfigurations = {
-  debugAll: boolean
-  debugEvents: boolean
-  debugContext: boolean
-}
-
-/**
- * Available options for events.
- * @public
- */
-export type EventsConfigurations = {
-  targetProperty: () => EventProperties[]
-}
-
-/**
  * All available configuration options.
  * @public
  */
 export type Configurations = {
-  logger: LoggerConfigurations
-  events: EventsConfigurations
+  debugAll: boolean
+  debugEvents: boolean
+  debugContext: boolean
+  targetProperty: () => EventProperties[]
 }
