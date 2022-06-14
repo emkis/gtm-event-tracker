3a7dc07e7c12db9560473bc2f7899d711cce6795
emkis <nicolasemkis@gmail.com>
2022-06-13 23:01:05 -0300

    docs: add description for config options

--- src/shared/configuration/configuration-types.ts
+++ src/shared/configuration/configuration-types.ts
@@ -5,8 +5,25 @@ import type { EventProperties } from '@/shared/data-layer'
  * @public
  */
 export type Configurations = {
+  /**
+   * It logs everything, is equivalent of setting all `debug` options to `true`.
+   */
   debugAll: boolean
+  /**
+   * It logs events. These events are logged when you call `trackEvent`.
+   */
   debugEvents: boolean
+  /**
+   * It logs context operations.
+   *
+   * These events are logged when you call `createTrackerContext`,
+   * and when you call `setProps` in a created tracker context.
+   */
   debugContext: boolean
+  /**
+   * Array which events are going to be pushed into.
+   *
+   * The default value is `window.dataLayer`.
+   */
   targetProperty: () => EventProperties[]
 }
