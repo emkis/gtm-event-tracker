b9062bbfb9ae8482bbf45cc8f2b2de132ac72cb0
emkis <nicolasemkis@gmail.com>
2023-03-02 21:55:02 +0000

    chore(scripts): add script to clean package.json

---
+++ scripts/clean-package-json.ts
@@ -0,0 +1,31 @@
+import packageJson from '../package.json'
+import { writeFile } from 'fs/promises'
+import { resolve } from 'node:path'
+import { format } from 'prettier'
+
+type PackageProperties = keyof typeof packageJson
+
+const unwantedProperties: PackageProperties[] = [
+  'keywords',
+  'files',
+  'scripts',
+  'devDependencies',
+]
+
+deleteProperties(unwantedProperties)
+rewritePackageJson()
+
+function deleteProperties(keys: string[]) {
+  keys.forEach((key) => delete packageJson[key])
+}
+
+async function rewritePackageJson() {
+  try {
+    const packagePath = resolve(__dirname, '..', 'package.json')
+    const content = JSON.stringify(packageJson)
+    const prettifiedContent = format(content, { parser: 'json-stringify' })
+    await writeFile(packagePath, prettifiedContent)
+  } catch {
+    console.error('Failed to rewrite the cleaned package.json file.')
+  }
+}
