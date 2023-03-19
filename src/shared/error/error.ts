13611dc4bda559df3ce7bc072f64619bfb89affc
emkis <nicolasemkis@gmail.com>
2023-03-19 15:03:01 +0000

    feat(error): throw minified error message

--- src/shared/error/error.ts
+++ src/shared/error/error.ts
@@ -1,6 +1,16 @@
+function createErrorMessage(message: string) {
+  if (process.env.NODE_ENV === 'production') {
+    const errorCode = message
+    return `Minified EventTrackerError error #${errorCode}. Visit https://github.com/emkis/gtm-event-tracker#error-codes for the full message.`
+  }
+
+  return message
+}
+
 export class EventTrackerError extends Error {
   constructor(message: string) {
-    super(message)
+    const parsedMessage = createErrorMessage(message)
+    super(parsedMessage)
     this.name = 'EventTrackerError'
   }
 }
